230fdeadc38c5a54440b91e6aa1106b6c77f55b3
xuanthuongcr <xuanthuongcr@gmail.com>
2014-11-05 21:23:23 +0700

    Add gen code

---
+++ Basic1/Basic1/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Basic1
+//
+//  Created by Ha Xuan Thuong on 11/5/14.
+//  Copyright (c) 2014 Ha Xuan Thuong. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
